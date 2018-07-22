7cc71b91dfce03e932a958dfd71c1d132b1f9642
Gregor Cox <gregorcox@gmail.com>
2018-07-22 21:37:30 +0100

    removes category routes from article controller

--- src/main/java/controllers/ArticleController.java
+++ src/main/java/controllers/ArticleController.java
@@ -119,26 +119,6 @@ public class ArticleController {
             return null;
         }, velocityTemplateEngine);
 
-//        get("/articles/category/:categoryName", (req, res) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            model.put("template", "templates/articles/category.vtl");
-//            Category category = Category.findByName("categoryName");
-//            List<Article> categoryArticles = DBHelper.getArticlesByCategory(category);
-//            model.put("categoryArticles", categoryArticles);
-//            model.put("category", category);
-//            return new ModelAndView(model, "templates/layout.vtl");
-//        }, velocityTemplateEngine);
-
-        get("/articles/category/science", (req, res) -> {
-            Map<String, Object> model = new HashMap();
-            model.put("template", "templates/articles/category.vtl");
-            List<Article> articles = DBHelper.getArticlesByCategory(Category.SCIENCE);
-            Category category = Category.SCIENCE;
-            model.put("articles", articles);
-            model.put("category", category);
-            return new ModelAndView(model, "templates/layout.vtl");
-        }, velocityTemplateEngine);
-
         get("/articles/:id", (request, response) -> {
             Map<String, Object> model = new HashMap();
             model.put("template", "templates/articles/show.vtl");
